495c12154bb3220499a7499eda874f6efb70375a
Fahad Devnikar <devnikarfahad@gmail.com>
2024-07-16 07:08:41 +0530

    implemented Transaction creation

--- prisma/migrations/20240716013011_init_schema/migration.sql
+++ prisma/migrations/20240716013011_init_schema/migration.sql
@@ -17,10 +17,10 @@ CREATE TABLE "Category" (
 CREATE TABLE "Transaction" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "amount" REAL NOT NULL,
     "description" TEXT NOT NULL,
-    "date" TEXT NOT NULL,
+    "date" DATETIME NOT NULL,
     "userId" TEXT NOT NULL,
     "type" TEXT NOT NULL DEFAULT 'income',
     "category" TEXT NOT NULL,
